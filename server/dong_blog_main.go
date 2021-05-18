d92f8afc3d89420b32b1ed2f69689cbdbc7f389b
dongdong <997546127@qq.com>
2021-05-19 00:38:14 +0800

    mvc的使用

--- server/dong_blog_main.go
+++ server/dong_blog_main.go
@@ -1,9 +1,56 @@
 package main
 
-import "fmt"
 import "github.com/kataras/iris/v12"
 
 func main() {
-	fmt.Print("赵东潮，来啊")
 	app := iris.New()
+
+	booksAPI := app.Party("/books")
+	{
+		//booksAPI.Use(iris.Gzip)
+
+		// GET: http://localhost:8080/books
+		booksAPI.Get("/", list)
+		// POST: http://localhost:8080/books
+		booksAPI.Post("/", create)
+	}
+
+	app.Listen(":8080")
+
+}
+
+// Book example.
+type Book struct {
+	Title string `json:"title"`
+}
+
+func list(ctx iris.Context) {
+	books := []Book{
+		{"Mastering Concurrency in Go"},
+		{"Go Design Patterns"},
+		{"Black Hat Go"},
+	}
+
+	ctx.JSON(books)
+	// TIP: negotiate the response between server's prioritizes
+	// and client's requirements, instead of ctx.JSON:
+	// ctx.Negotiation().JSON().MsgPack().Protobuf()
+	// ctx.Negotiate(books)
+}
+
+func create(ctx iris.Context) {
+	var b Book
+	err := ctx.ReadJSON(&b)
+	// TIP: use ctx.ReadBody(&b) to bind
+	// any type of incoming data instead.
+	if err != nil {
+		ctx.StopExecution()
+		// TIP: use ctx.StopWithError(code, err) when only
+		// plain text responses are expected on errors.
+		return
+	}
+
+	println("Received Book: " + b.Title)
+
+	ctx.StatusCode(iris.StatusCreated)
 }
--- server/go.mod
+++ server/go.mod
@@ -1,8 +1,10 @@
 module d-blog
+
 // 指定go的版本
 go 1.16
+
 //GOPROXY=https://goproxy.cn,https://gocenter.io,https://goproxy.io,direct
 require (
-	github.com/kataras/iris/v12 latest
-
+	github.com/kataras/iris/v12 v12.1.8
+	github.com/shurcooL/sanitized_anchor_name v1.0.0
 )
